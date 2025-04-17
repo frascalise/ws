c014ba5c0764f0fed835f9216cf76be7b1c535cf
frascalise <scalise92111@gmail.com>
2025-04-17 21:46:09 +0200

    Edit home page

--- api/urls.py
+++ api/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from .views import home
+from django.urls import path, include, re_path
+
+from .views import home, not_found_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+
+    re_path(r'^.*$', not_found_view, name='not_found'),  # Catch-all for any other URL
 ]
--- api/views.py
+++ api/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/home.html')
+
+# Custom 404 view
+def not_found_view(request):
+    message = "Oops! The page you're looking for doesn't exist or has been moved."
+    return render(request, 'error/error.html', {'status_code': 404, 'message': message}, status=404)
